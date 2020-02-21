61ef89b4be3cc34a7ec57c9a08d6833cd27eb9f1
Doni Setiawan <donisetiawan.050@gmail.com>
2020-02-21 15:32:11 +0700

    Closing the server

---
+++ kqy.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+let closed = false;
+
+app.get('/', (req, res) => {
+  if (!closed) {
+    server.close();
+
+    res.send('First request');
+
+    closed = true;
+  } else {
+    res.send('Server shutting down ...');
+  }
+
+  setTimeout(() => {
+    process.exit();
+  }, 5000);
+});
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
